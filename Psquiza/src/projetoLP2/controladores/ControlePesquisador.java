e516df90d3d819c45361dd3545cddf649318971a
MelquiSilva <48794046+MelquiSilva@users.noreply.github.com>
2019-10-29 18:11:52 -0300

    Update ControlePesquisador.java
    adicionei a documentação de ControlePesquisador

--- Psquiza/src/projetoLP2/controladores/ControlePesquisador.java
+++ Psquiza/src/projetoLP2/controladores/ControlePesquisador.java
@@ -5,13 +5,31 @@ import projetoLP2.util.Verificador;
 
 import java.util.HashMap;
 
+/**
+ * Classe responsavel por representar um controle que admnistra pesquisadores.
+ */
 public class ControlePesquisador {
+    /**
+     * Atributo que representa um Mapa contendo tipo String e tipo Pesquisador.
+     * O tipo String é o email do pesquisador, que serve como identificador.
+     */
     private HashMap<String, Pesquisador> pesquisadores;
 
+    /**
+     * Constroi o HahsMap pesquisadores.
+     */
     public ControlePesquisador(){
         pesquisadores = new HashMap<>();
     }
 
+    /**
+     * Metodo responsavel por cadastrar pesquisadores no controle.
+     * @param nome representa o nome do pesquisador a ser cadastrado.
+     * @param funcao representa a funcao do pesquisador a ser cadastrado.
+     * @param biografia representa a biografia do pesquisador a ser cadastrado.
+     * @param email representa o email do pesquisador a ser cadastrado.
+     * @param fotoURL representa a url da foto do pesquisador a ser cadastrado.
+     */
     public void cadastraPesquisador(String nome, String funcao, String biografia, String email, String fotoURL){
         Verificador.verificaString("Campo nome nao pode ser nulo ou vazio.", nome);
         Verificador.verificaString("Campo funcao nao pode ser nulo ou vazio.", funcao);
@@ -23,18 +41,35 @@ public class ControlePesquisador {
             throw new IllegalArgumentException("Formato de email invalido.");
         }else if(! fotoURL.startsWith("http://") & ! fotoURL.startsWith("https://")){
             throw new IllegalArgumentException("Formato de foto invalido.");
-        }else if(pesquisadores.containsKey(email)){
-            throw new IllegalArgumentException();
         }else{
             pesquisadores.put(email, new Pesquisador(nome, funcao, biografia, email, fotoURL));
         }
     }
 
-    public void alteraPesquisador(String email, String atributo, String novoValor){
+    /**
+     * Metodo responsavel por alterar dados dos pesquisadores no controle.
+     * @param email representa o email do pesquisador
+     * @param atributo representa o atributo que determinado pesquisador quer alterar.
+     * @param novoValor representa o novo valor para o atributo desejado.
+     */
+    public void alteraPesquisador(String email, String atributo, String novoValor) {
         Verificador.verificaString("Campo email nao pode ser nulo ou vazio.", email);
         Verificador.verificaString("Campo atributo nao pode ser nulo ou vazio.", atributo);
         Verificador.verificaString("Campo " + atributo + " nao pode ser nulo ou vazio.", novoValor);
-        if(! pesquisadores.containsKey(email)){
+        if(atributo.equals("email")){
+            if(! novoValor.contains("@") || novoValor.startsWith("@") || novoValor.endsWith("@")) {
+                throw new IllegalArgumentException("Formato de email invalido.");
+            }else {
+                pesquisadores.put(novoValor, new Pesquisador(pesquisadores.get(email).getNome(),pesquisadores.get(email).getFuncao(),pesquisadores.get(email).getBiografia(),novoValor,pesquisadores.get(email).getFotoURL()));
+                pesquisadores.remove(email);
+            }
+        } else if(atributo.equals("fotoURL")) {
+            if (! novoValor.startsWith("http://") & !novoValor.startsWith("https://")) {
+                throw new IllegalArgumentException("Formato de foto invalido.");
+            } else {
+                pesquisadores.get(email).setFotoURL(novoValor);
+            }
+        } else if(! pesquisadores.containsKey(email)){
             throw new IllegalArgumentException("Pesquisador nao encontrado");
         } else if(pesquisadores.get(email).isAtivado() == false){
             throw new IllegalArgumentException("Pesquisador inativo.");
@@ -45,23 +80,15 @@ public class ControlePesquisador {
                 pesquisadores.get(email).setFuncao(novoValor);
             }else if(atributo.equals("biografia")){
                 pesquisadores.get(email).setBiografia(novoValor);
-            }else if(atributo.equals("email")){
-                if(! novoValor.contains("@") || novoValor.startsWith("@") || novoValor.endsWith("@")){
-                    throw new IllegalArgumentException("Formato de email invalido.");
-                }else{
-                    pesquisadores.get(email).setEmail(novoValor);
-                }
-            }else if(atributo.equals("fotoURL")){
-                if(! novoValor.startsWith("http://") & ! novoValor.startsWith("https://")){
-                    throw new IllegalArgumentException("Formato de foto invalido.");
-                }else{
-                    pesquisadores.get(email).setFotoURL(novoValor);
-                }
+
             }
         }
     }
 
-
+    /**
+     * Metodo responsavel por desativar pesquisadores no controle.
+     * @param email representa o email do pesquisador.
+     */
     public void desativaPesquisador(String email){
         Verificador.verificaString("Campo email nao pode ser nulo ou vazio.", email);
         if(! email.contains("@") || email.startsWith("@") || email.endsWith("@")){
@@ -74,6 +101,11 @@ public class ControlePesquisador {
             pesquisadores.get(email).setAtivado(false);
         }
     }
+
+    /**
+     * Metodo responsavel por ativar pesquisadores no controle.
+     * @param email representa o email do pesquisador.
+     */
     public void ativaPesquisador(String email){
         Verificador.verificaString("Campo email nao pode ser nulo ou vazio.", email);
         if(! email.contains("@") || email.startsWith("@") || email.endsWith("@")) {
@@ -86,6 +118,12 @@ public class ControlePesquisador {
             pesquisadores.get(email).setAtivado(true);
         }
     }
+
+    /**
+     * Metodo responsavel por exibir pesquisadores.
+     * @param email representa o email do pesquisador.
+     * @return representacao em string dos dados do pesquisador.
+     */
     public String exibePesquisador(String email) {
         Verificador.verificaString("Campo email nao pode ser nulo ou vazio.", email);
         if(! email.contains("@") || email.startsWith("@") || email.endsWith("@")) {
@@ -98,8 +136,14 @@ public class ControlePesquisador {
             return pesquisadores.get(email).toString();
         }
     }
+
+    /**
+     * Metodo responsavel por retornar se o pesquisador esta ativo.
+     * @param email representa o email do pesquisador.
+     * @return representacao em boolean, true para pesquisador ativo e false para pesquisador inativo.
+     */
     public boolean pesquisadorEhAtivo(String email){
-        Verificador.verificaString("Campo email nao pode ser nulo ou vazio.", email);
+        Verificador.verificaString("Email nao pode ser vazio ou nulo.", email);
         if(! email.contains("@") || email.startsWith("@") || email.endsWith("@")) {
             throw new IllegalArgumentException("Formato de email invalido.");
         }else if(! pesquisadores.containsKey(email)){
