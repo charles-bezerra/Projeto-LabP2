9cf12fd648652d53bce1c8977593533ea09c9fca
119110595 CHARLES BEZERRA DE OLIVEIRA JUNIOR <charlesboj@lcc3-103.lcc.ufcg.edu.br>
2019-11-05 09:53:22 -0300

    Teste de unidade da US1 feito 50%

--- Psquiza/testes/unidade/classes/PesquisaTest.java
+++ Psquiza/testes/unidade/classes/PesquisaTest.java
@@ -12,11 +12,50 @@ class PesquisaTest {
 
     @BeforeEach
     void criarObjetos(){
+        this.pesquisa1 = new Pesquisa("Uma simples pesquisa academica", "Estudo");
+        this.pesquisa2 = new Pesquisa("Uma complexa pesquisa academica","Estudo");
+    }
 
+
+    @Test
+    void criaComDescricaoNula(){
+        try {
+            Pesquisa pesquisa = new Pesquisa(null, "Medicina");
+        }catch (IllegalArgumentException erro){
+            assertEquals("java.lang.IllegalArgumentException: Descricao nao pode ser nula ou vazia.", erro.toString());
+        }
+    }
+
+    @Test
+    void criaComDescricaoVazia(){
+        try {
+            Pesquisa pesquisa = new Pesquisa("", "Medicina");
+        }catch (IllegalArgumentException erro){
+            assertEquals("java.lang.IllegalArgumentException: Descricao nao pode ser nula ou vazia.", erro.toString());
+        }
+    }
+
+    @Test
+    void criaComCampoDeInteresseNulo(){
+        try {
+            Pesquisa pesquisa = new Pesquisa("Um breve estudo sobre o efeito de altas temperaturas em idosos", null);
+        }catch (IllegalArgumentException erro){
+            assertEquals("java.lang.IllegalArgumentException: Formato do campo de interesse invalido.", erro.toString());
+        }
+    }
+
+    @Test
+    void criaComCampoDeInteresseVazio(){
+        try {
+            Pesquisa pesquisa = new Pesquisa("Um breve estudo sobre o efeito de altas temperaturas em idosos", "");
+        }catch (IllegalArgumentException erro){
+            assertEquals("java.lang.IllegalArgumentException: Formato do campo de interesse invalido.", erro.toString());
+        }
     }
 
     @Test
     void setDescricao() {
+
     }
 
     @Test
