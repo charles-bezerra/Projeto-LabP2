ba531eb899f292b48bfcfeb4fa88647d0bffcf1a
iagohss <iago.silva@ccc.ufcg.edu.br>
2019-11-13 18:49:00 -0300

    Adicionei os novos testes de aceitação

--- Psquiza/src/projetoLP2/facades/Facade.java
+++ Psquiza/src/projetoLP2/facades/Facade.java
@@ -182,7 +182,12 @@ public class Facade {
                 "testes/aceitacao/use_case_5.txt",
                 "testes/aceitacao/use_case_6.txt",
                 "testes/aceitacao/use_case_7.txt",
-                "testes/aceitacao/use_case_8.txt"
+                "testes/aceitacao/use_case_8.txt",
+                "testes/aceitacao/use_case_9.txt",
+                "testes/aceitacao/use_case_10.txt",
+                "testes/aceitacao/use_case_11.txt",
+                "testes/aceitacao/use_case_12SALVAR.txt",
+                "testes/aceitacao/use_case_12CARREGAR.txt"
         };
         EasyAccept.main(args);
     }
