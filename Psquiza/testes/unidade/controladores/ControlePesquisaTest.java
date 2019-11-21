5e747abdcc06d5abe5ab1296f74f7675ef44af22
MelquiSilva <melquisedeque.silva@ccc.ufcg.edu.br>
2019-11-21 06:44:56 -0300

    testes us10

--- Psquiza/testes/unidade/controladores/ControlePesquisaTest.java
+++ Psquiza/testes/unidade/controladores/ControlePesquisaTest.java
@@ -6,47 +6,45 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import projetoLP2.classes.Atividade;
 
+import projetoLP2.classes.Pesquisa;
 import projetoLP2.controladores.ControlePesquisa;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControlePesquisaTest {
-    private Atividade a1, a2;
+    private Atividade a1, a2, a3, a4, a5;
     private Exception e;
 
     private ControlePesquisa pesquisas;
-    private String pesquisa1;
+    //private String pesquisa1;
+    private Pesquisa p1, p2;
 
     @BeforeEach
-    void criaObjetos(){
+    void criaObjetos() {
         this.pesquisas = new ControlePesquisa();
 
-        this.pesquisa1 = this.pesquisas
-                .cadastraPesquisa(
-                        "Estuda da atmosfera brasileira.",
-                        "Computação"
-                );
 
-        a1 = new Atividade(
-                "Uma simples atividade",
-                "BAIXO",
-                "E simples, entao e facil."
-        );
+        a1 = new Atividade("Uma simples atividade", "BAIXO", "E simples, entao e facil.");
+        a2 = new Atividade("Uma atividade complicada", "ALTO", "E dificil, porque nao sei.");
+        a3 = new Atividade("Atividade boa", "ALTO", "boa, mas nem tanto");
+        a4 = new Atividade("Boa atividade", "MEDIO", "ate que da");
+        a5 = new Atividade("Mais ou menos", "BAIXO", "pensando bem");
 
-        a2 = new Atividade(
-                "Uma atividade complicada",
-                "ALTO",
-                "E dificil, porque nao sei.");
+        p1 = new Pesquisa("teste", "TESTE");
+        p2 = new Pesquisa("testando", "junit");
 
-        pesquisas.cadastraPesquisa(
-                "teste",
-                "TESTE");
+        a3.cadastraItem("O meu primeiro item");
+        a3.cadastraItem("O meu segundo item");
+        a3.executaAtividade(1, 10);
+        a3.executaAtividade(2,15);
+        a3.cadastraResultado("Deu certo");
+        a3.cadastraResultado("não deu muito certo");
     }
 
     @Test
-    void cadastraPesquisa(){
+    void cadastraPesquisa() {
         assertEquals(
-                "COM2",
+                "COM1",
                 this.pesquisas
                         .cadastraPesquisa(
                                 "Estuda de computação",
@@ -69,8 +67,7 @@ class ControlePesquisaTest {
                     "Computação"
             );
             fail();
-        }
-        catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             assertEquals("java.lang.IllegalArgumentException: Descricao nao pode ser nula ou vazia.",
                     e.toString());
         }
@@ -84,8 +81,7 @@ class ControlePesquisaTest {
                     "Computação"
             );
             fail();
-        }
-        catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             assertEquals(
                     "java.lang.IllegalArgumentException: Descricao nao pode ser nula ou vazia.",
                     e.toString()
@@ -102,6 +98,33 @@ class ControlePesquisaTest {
     void encerraPesquisa() {
     }
 
+    @Test
+    void associaAtividadeTrue() {
+        assertTrue(p2.associaAtividade(a1));
+        assertTrue(p2.associaAtividade(a2));
+        assertTrue(p2.associaAtividade(a3));
+
+    }
+
+    @Test
+    void cadastraItem() {
+        a1.cadastraItem("O meu primeiro item");
+        a2.cadastraItem("O meu segundo item");
+    }
+
+    @Test
+    void cadastraItemVazioOuNulo() {
+        e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            a1.cadastraItem(null);
+        });
+        assertEquals("Item nao pode ser nulo ou vazio.", e.getMessage());
+        e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            a1.cadastraItem("");
+        });
+        assertEquals("Item nao pode ser nulo ou vazio.", e.getMessage());
+    }
+
+    
     @Test
     void ativaPesquisa() {
     }
@@ -123,23 +146,8 @@ class ControlePesquisaTest {
     }
 
     @Test
-    void associaProblema() {
-    }
-
-    @Test
-    void desassociaProblema() {
-    }
-
-    @Test
-    void associaObjetivo() {
-    }
-
-    @Test
-    void desassociaObjetivo() {
-    }
-
-    @Test
     void listaPesquisas() {
+
     }
 
     @Test
@@ -170,14 +178,14 @@ class ControlePesquisaTest {
                 e.getMessage()
         );
 
+        pesquisas.cadastraPesquisa(p1.getDescricao(), p1.getCampoDeInteresse());
+        assertTrue(p1.associaAtividade(a1));
+        assertFalse(p1.associaAtividade(a1));
 
-        assertTrue(pesquisas.associaAtividade("TES1",a1));
-        assertFalse(pesquisas.associaAtividade("TES1",a1));
-
-        pesquisas.encerraPesquisa("TES1","AAAA");
+        p1.encerraPesquisa(p1.getCod());
 
         e = Assertions.assertThrows(IllegalArgumentException.class, () ->
-                pesquisas.associaAtividade("TES1", a1));
+                p1.associaAtividade(a1));
 
         assertEquals("Pesquisa desativada.", e.getMessage());
     }
@@ -199,23 +207,23 @@ class ControlePesquisaTest {
 
         assertEquals("Pesquisa nao encontrada.", e.getMessage());
 
-        pesquisas.associaAtividade("TES1",a1);
+        p1.associaAtividade(a1);
 
-        assertTrue(pesquisas.desassociaAtividade("TES1",a1));
+        assertTrue(p1.desassociaAtividade(a1));
 
-        assertFalse(pesquisas.desassociaAtividade("TES1",a1));
+        assertFalse(p1.desassociaAtividade(a1));
 
-        pesquisas.encerraPesquisa("TES1","AAAA");
+        p1.encerraPesquisa("AAAA");
 
         e = assertThrows(IllegalArgumentException.class, () ->
-                pesquisas.desassociaAtividade("TES1", a1));
+                p1.desassociaAtividade(a1));
 
         assertEquals(
                 "Pesquisa desativada.",
                 e.getMessage()
         );
 
-        pesquisas.ativaPesquisa("TES1");
+        p1.ativaPesquisa();
     }
 
     @Test
